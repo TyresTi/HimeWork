e56a043578a63f9c8e1fc8666e606a1ecfe31413
vvdob <vvdobryninskiy@gmail.com>
2026-08-05 21:59:32 +0300

    hw 18/07/26

--- src/Main.java
+++ src/Main.java
@@ -1,106 +1,35 @@
+import java.util.Arrays;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
         // Task №1
-        System.out.println("//Task №1");
-        int firstFriday = 1;
-        for (; firstFriday <= 31; firstFriday += 7) {
-            System.out.println("Сегодня пятница, " + firstFriday + "-е число. Необходимо подготовить отчет.");
+        int[] weight = new int[3];
+        weight[0] = 3;
+        weight[1] = 9;
+        weight[2] = 67;
+        // Task №1.1
+        float[] weight1 = {1.57F, 7.654F, 9.986F};
+        // Task №1.2
+        String[] weight2 = {"Январь", "Февраль", "Март", "Апрель", "Май", "Июнь" };
+        // Task №2
+        System.out.println("Task №2");
+        System.out.println(weight[0] + "," + weight[1] + "," + weight[2]);
+        System.out.println(weight1[0] + "," + weight1[1] + "," + weight1[2]);
+        System.out.println(weight2[0] + "," + weight2[1] + "," + weight2[2] + "," + weight2[3] + "," + weight2[4] + "," + weight2[5]);
+        // Task №3
+        System.out.println("Task №3");
+        System.out.println(weight[2] + "," + weight[1] + "," + weight[0]);
+        System.out.println(weight1[2] + "," + weight1[1] + "," + weight1[0]);
+        System.out.println(weight2[5] + "," + weight2[4] + "," + weight2[3] + "," + weight2[2] + "," + weight2[1] + "," + weight2[0]);
+        // Task №4
+        System.out.println("Task №4");
+        for (int i = 0; i < weight.length; i++) {
+            weight[i]++;
+            System.out.println(weight[i]);
         }
-
-        //Task №2
-        System.out.println("//Task №2");
-        int distance = 0;
-        int fullDistance = 42195;
-        do {
-            System.out.println("Держитесь! Осталось " + (fullDistance - distance) + " метров");
-            distance = distance + 500;
-        }
-        while (distance <= fullDistance);
-
-        //Task №2.1
-        System.out.println("//Task №2.1");
-        int fulldistance1 = 42195;
-        int steps = 0;
-        for (int distance1 = 0; distance1 < +fulldistance1; distance1 += 500) {
-            steps = steps + 1;
-            System.out.println("Держитесь! Осталось " + (fulldistance1 - distance1) + " метров");
-        }
-
-        //Task №2.2
-        System.out.println("//Task №2.2");
-        System.out.println("Программа работает корректно и выводит " + steps + " сообщения");
-
-        //Task №2.3
-        System.out.println("//Task №2.3");
-        System.out.println("Оба варианта проверены на читаемость и корректность кода");
-
-        //Task №3
-        System.out.println("//Task №3");
-        int sum = 1000;
-        int currentDay = 1;
-        while (sum >= 100) {
-            if (currentDay % 5 == 0) {
-                currentDay++;
-                continue;
-            }
-            sum = sum - 100;
-            currentDay++;
-        }
-        System.out.println("Вы можете оставить автомобиль на " + (currentDay - 1) + " дней.");
-        //Task №3.1
-        System.out.println("//Task №3.1");
-        int sum1 = 1000;
-        int currentDay1 = 1;
-        for (int day = 1; sum1 >= 100; day++) {
-            if (day % 5 == 0) {
-                currentDay1++;
-                continue;
-            }
-            sum1 = sum1 - 100;
-            currentDay1++;
-        }
-        System.out.println("Вы можете оставить автомобиль на " + (currentDay1 - 1) + " дней.");
-
-        //Task №4
-        System.out.println("//Task №4");
-        int month = 0;
-        int total = 0;
-        while (true) {
-            total += 15000;
-            month++;
-            if (month % 6 == 0) {
-                total += total * 7 / 100;
-            }
-            System.out.println("Месяц " + month + ". Сумма накоплений: " + total);
-            if (total >= 12_000_000) {
-                break;
-            }
-        }
-
-        //Task №5
-        System.out.println("//Task №5");
-        int charge = 20;
-        int minute = 0;
-        int overheats = 0;
-        while (charge < 100) {
-            minute++;
-            if (minute % 10 == 0) {
-                overheats++;
-                System.out.println("Перегрев! Зарядка приостановлена.");
-
-                if (overheats >= 3) {
-                    System.out.println("Зарядка досрочно завершена.");
-                    break;
-                }
-                minute += 2;
-                continue;
-            }
-            charge += 2;
-        }
-        System.out.println("Время зарядки составило " + minute + " минут.");
     }
 }
 
