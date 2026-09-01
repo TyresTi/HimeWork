e5a2ddb7a66340dafea762b8091bae75b137edd8
DobrininskyVV <vvdobryninskuy@gmail.com>
2026-09-01 13:05:40 +0300

    Homework to check

--- src/Main.java
+++ src/Main.java
@@ -5,23 +5,7 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
 //        Task №1
-        System.out.println("Task №1");
-        String firstName = "Ivan";
-        String middleName = "Ivanovich";
-        String lastName = "Ivanov";
-        String fullName = "Ivanov Ivan Ivanovich";
-        System.out.println("Ф. И. О. сотрудника — " + fullName);
-//        Task №2
-        System.out.println("Task №2");
-        String f1 = fullName.toUpperCase();
-        System.out.println("Данные Ф. И. О. сотрудника для заполнения отчета — " + f1);
-//        Task №3
-        System.out.println("Task №3");
-        String fullName1 = "Иванов Семён Семёнович";
-        fullName1 = fullName1.replace("ё", "е");
-        System.out.println("Данные Ф. И. О. сотрудника — " + fullName1);
-//        Task №4
-        System.out.println("Task №4");
+
     }
 }
 
